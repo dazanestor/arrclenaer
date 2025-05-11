bf3f84cd5dc3698b88456ebfb74ec5ebf76c79a9
dazanestor <dazanestor@gmail.com>
2025-05-11 19:28:43 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,12 @@ source /home/appuser/venv/bin/activate
 # Log para el inicio
 echo "[INFO] Iniciando ciclo con intervalo de ${INTERVAL_HOURS} horas..."
 
+# Revisar el valor de INTERVAL_HOURS
+if [ -z "$INTERVAL_HOURS" ]; then
+    echo "[ERROR] La variable INTERVAL_HOURS no está definida. Usando valor predeterminado: 12"
+    INTERVAL_HOURS=12  # Establecer un valor por defecto si no está definida
+fi
+
 # Ciclo de ejecución
 while true; do
     echo "[INFO] Ejecutando limpieza a $(date)"
